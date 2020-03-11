f6c1a9bc67e71c35af0d6cb0da13d82353366f70
prashathc <prashathc@gmail.com>
2020-03-11 11:43:38 -0400

    factory functions vs constructor functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('wsup')
+// Factories
+
+// If an object has one or more methods, we have that object has behaviour. 
+// Creating objects with object literal syntax is only an issue if that object has more than one method, because it is redudunant
+
+// Solution is to use a factory or constructor function
+
+// Factory Function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('draw')
+        }
+    }
+}
+const circle = createCircle(1);
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw')
+    }
+}
+
+const anotherCircle = new Circle(1);
+
+
+
