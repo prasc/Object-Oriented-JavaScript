75f8c2eb0d3df887762aac9fc6cbbeb1bbe0efa1
prashathc <prashathc@gmail.com>
2020-03-12 19:51:31 -0400

    inerhitance

---
+++ inheritance.js
@@ -0,0 +1,7 @@
+// Prototypical Inheritance (different from Classical)
+
+// every object has a prototype or parent
+
+// every object has a constructor property which references the function that was used to construct or create that object
+
+// A prototype is just a regular object in memory! 
---
+++ multi-level-inheritance.js
@@ -0,0 +1,4 @@
+// Everything is an object in JS
+// Because everything that is not an object, derives from an object that dervices from the object base
+
+// This applies to arrays and functions
